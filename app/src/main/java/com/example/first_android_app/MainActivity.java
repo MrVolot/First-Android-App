48a463cec73c936171368d5e04fe1086b8032efb
Mr_Volot <kiril-kiril2003@ukr.net>
2021-09-20 18:35:00 +0300

    First button and text were added

--- app/src/main/java/com/example/first_android_app/MainActivity.java
+++ app/src/main/java/com/example/first_android_app/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.first_android_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +14,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void onClick(View view){
+        TextView text = (TextView) findViewById(R.id.textView);
+        text.setText("Button was clicked");
+    }
+
 }
